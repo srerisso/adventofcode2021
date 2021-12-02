2c21ff1bf781138d85f4868df29bd2ad842c61bb
Sr Erisso <works@erisso.com>
2021-12-02 17:50:10 +0000

    Day 1 Part 2. I still haven't found what I'm looking for ...

--- p1/day1.py
+++ p1/day1.py
@@ -7,19 +7,25 @@
 # there's a max depth reading.
 def find_increases(list):
     deepReadings = []
+    incDeeps = []
     max = 0
+    counter = 0
 
     for el in list:
         if el >= max:
+            incDeeps.append(el)
             max = el
         else:
-            deepReadings.append(max)
+            deepReadings.append(el)
+            counter = counter+1
             max = el
 
-    print("The total number measurements is ", len(intlines_list)) 
-    print('There are deep measurements measurements ', len(deepReadings)) 
+    print("The total number measurements is ", len(list)) 
+    print('Total decreasing measurements ', len(deepReadings)) 
+    print('Total incresing measurements ', len(incDeeps)) 
     print('The total number of measurements larger than the previous is ') 
-    print(len(lines)-len(deepReadings))
+    print(len(list)-len(deepReadings))
+    print('Counter: ', counter)
 
 #    return(len(lines)-len(deepReadings))
     return(deepReadings)
@@ -45,7 +51,7 @@ with open('input', 'r') as f:
 integer_lines = map(int, lines)
 intlines_list = list(integer_lines)
 #print("Total number of MAX depth readings: ", len(lines)-len(find_increases(intlines_list)))
-find_increases(intlines_list)
+print(find_increases(intlines_list))
 
 ## PART 2 ##
 # Sum window of n elements (window)
@@ -58,13 +64,16 @@ window = 3
 
 newlen = len(lines)
 #print("New length: ", newlen)
-newlen = newlen - (len(lines) % window)
+#newlen = newlen - (len(lines) % window)
 #print("New Length updated: ", newlen)
 
 for elem in range(0,newlen):
-    intervalReadings.append(lines[i:j])
-    i = i+1
-    j = j+1
+    if j == (newlen+1):
+        break
+    else:
+        intervalReadings.append(lines[i:j])
+        i = i+1
+        j = j+1
 
 # We have created intervalReadings, a list of sublists of lines. Each sublist has 3 (window) elements
 # Now, we need the sum of each 3 elements.
@@ -77,4 +86,4 @@ intervalSumReadings = map(suml, intervalReadings)
 print('*** ******************************** ***')
 #print(len(intlines_list)-len(find_increases(list(intervalSumReadings))))
 #print("Total number of MAX depth readings (Window system): ", find_increases(list(intervalSumReadings)))
-find_increases(list(intervalSumReadings))
+print(find_increases(list(intervalSumReadings)))
