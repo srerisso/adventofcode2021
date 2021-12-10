18f271a2415aa0b6061c793f4c57ccd9896032c1
Sr Erisso <works@erisso.com>
2021-12-10 11:29:04 +0000

    Added some comments to Part 1..

--- p2/day2.py
+++ p2/day2.py
@@ -9,13 +9,16 @@ with open('submarine_course_input', 'r') as f:
     course_lines = f.read().splitlines()
 
 
-# negative. Turn integer int a negative integer integer.
+# Function negative. Turn integer int a negative integer integer.
 def negative(number):
     negative_number = '-' + str(number)
     return int(negative_number)
 
+## DAY 2. PART 1
 # Create and initialize a list that updates the coordinates' values, position (F) and depth (D). [F,D]
 position = [0,0]
+# Additionally, create two lists to save the values of horizontal and depth. The normal should be to save depth readings as negative values (every down command)
+# and every up command as a positive vlaue. But I'll do it the other way. Not important, just keep in mind the signs.
 horizontal = []
 depth = []
 
@@ -23,7 +26,6 @@ depth = []
 # forward X : increase F
 # down X : increase D
 # up X : decrease D
-
 for el in course_lines:
     if (el.split()[0]) == 'forward':
         position[0] = position[0] + int(el.split()[1])
@@ -39,3 +41,6 @@ print('Position', position)
 #print('Horizontal ', horizontal)
 #print('Depth', depth)
 print('Multiplication', position[0]*position[1])
+
+
+## DAY 2. PART 2
