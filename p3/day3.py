dc0f0a1011d51d94be87947dc711f1ad64f1a24e
Sr Erisso <works@erisso.com>
2021-12-10 16:45:34 +0000

    Day 3 START.

---
+++ p3/day3.py
@@ -0,0 +1,15 @@
+## AOC. DAY 3.
+# Binary Diagnostic
+
+# power_consumption = gamma_rate * epsilon_rate
+
+# diagnostic_report (input)
+
+# Open file with diagnostic report values
+with open('diagnostic_input', 'r') as f:
+    # File processing
+    diagnostic_lines = f.read().splitlines()
+
+print('Diagnostic values: {}'.format(diagnostic_lines))
+
+
