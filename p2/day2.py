a9ccd95fb5b2219a47cb794e16fcd788838238fe
Sr Erisso <works@erisso.com>
2021-12-10 12:05:55 +0000

    Day 2. Part 2, finished.

--- p2/day2.py
+++ p2/day2.py
@@ -8,7 +8,6 @@ with open('submarine_course_input', 'r') as f:
     # File processing
     course_lines = f.read().splitlines()
 
-
 # Function negative. Turn integer int a negative integer integer.
 def negative(number):
     negative_number = '-' + str(number)
@@ -18,7 +17,7 @@ def negative(number):
 # Create and initialize a list that updates the coordinates' values, position (F) and depth (D). [F,D]
 position = [0,0]
 # Additionally, create two lists to save the values of horizontal and depth. The normal should be to save depth readings as negative values (every down command)
-# and every up command as a positive vlaue. But I'll do it the other way. Not important, just keep in mind the signs.
+# and every up command as a positive value. But I'll do it the other way. Not important, just keep in mind the signs.
 horizontal = []
 depth = []
 
@@ -37,10 +36,34 @@ for el in course_lines:
         position[1] = position[1] - int(el.split()[1])
         depth.append(negative(el.split()[1]))
 
-print('Position', position)
+print('POSITION: ', position)
 #print('Horizontal ', horizontal)
 #print('Depth', depth)
-print('Multiplication', position[0]*position[1])
+print('PART 1. MULTIPLICATION: ', position[0]*position[1])
 
 
 ## DAY 2. PART 2
+# New interpretation of commands.
+# up X : decrease AIM by X units
+# down X : increase AIM by X units
+# forward X : increase horizontal AIM by X units
+position2 = [0,0]
+horizontal2 = []
+depth2 = []
+aim = 0
+aims = [0]
+
+for read in course_lines:
+    if (read.split()[0]) == 'up':
+        aim = aim - int(read.split()[1])
+        aims.append(int(read.split()[1]))
+    if (read.split()[0]) == 'down':
+        aim = aim + int(read.split()[1])
+        aims.append(int(read.split()[1]))
+    if (read.split()[0]) == 'forward':
+        position2[0] = position2[0] + int(read.split()[1])
+        position2[1] = position2[1] + aim * int(read.split()[1])
+
+print('AIM values: ', aims)
+print('POSITION 2: ', position2)
+print('PART 2. MULTIPLICATION: ', position2[0]*position2[1])
