64f8464917bf386fe4d3318409c4303214631534
Sr Erisso <works@erisso.com>
2021-12-14 17:22:00 +0000

    Day 3 working.

--- p3/day3.py
+++ p3/day3.py
@@ -12,23 +12,17 @@ with open('diagnostic_input', 'r') as f:
 
 print('Diagnostic values: {}'.format(diagnostic_lines))
 
-bitlist0 = []
-bitlist1 = []
-bitlist2 = []
-bitlist3 = []
-bitlist4 = []
-bitlist5 = []
-bitlist6 = []
-bitlist7 = []
-bitlist8 = []
-bitlist9 = []
-bitlist10 = []
-bitlist11 = []
+bitlist = []
+strofbits = ''
+count = 0
 
 # Here is the chicha
 for element in diagnostic_lines: # Loop through all elements of diagnostic_lines
     for bit in range(0,len(element)-1):  # Loop through all 12 bitlists of every element
-        #bitlist0[bit].append(element[bit])
-        print(bit)
+        element[bit]
+    strofbits = strofbits + element[bit]
+    bitlist[count] = strofbits
+    count += 1
 
-print(bitlist0)
+
+print(bitlist)
